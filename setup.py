d6004b8e3a03e031bcc247c75eef4653b2e23b71
kotani-py <kotani.takumi@g.wakayama-u.jp>
2022-06-22 11:17:45 +0900

    v3.1.0リリース準備

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = pathlib.Path(__file__).parent.resolve()
 
 setup(
     name='nucosen',
-    version='3.1.0a1',
+    version='3.1.0',
     description='Broadcasting system for NUCOSen',
     url='https://github.com/nucosen/Broadcast',
     author='NUCOSen Management Committee',
