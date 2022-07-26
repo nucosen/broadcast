1596a7405da9ac3921aee6022a01f4e94b2546ba
NUCO <93378833+sitting-cat@users.noreply.github.com>
2022-07-26 09:54:20 +0900

    パッケージのプロパティとしてライセンス情報を明記 Fixes #56

--- src/nucosen/__init__.py
+++ src/nucosen/__init__.py
@@ -14,3 +14,5 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+
+__version__ = "3.2.0"
